01a162fa84ff0546347d3b58767b37ffbb888d8c
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-03-04 19:22:58 +0530

    Routing and Nested Routing

---
+++ app/about/user/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const user = () => {
+  return (
+    <div>This is user page inside about</div>
+  )
+}
+
+export default user
---
+++ app/about/user/projects/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>this is Project page inside about inside user </div>
+  )
+}
+
+export default Project
