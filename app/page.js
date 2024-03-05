8f592b5feef3457875d790b47eec3b907f95c00b
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-03-05 13:48:45 +0530

    useRouter hook

--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
+'use client';
+
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const Home = () => {
+
+  const router = useRouter();
+
+  const navigate = (page) =>{
+      router.push(page);
+  }
   return (
-    <div>Home</div>
+    <div>This is the Home Page of this App
+    <button className="bg-green-500 border m-2 p-2" onClick={()=>navigate('about')}>About button</button>
+    </div>
   )
 }
 
