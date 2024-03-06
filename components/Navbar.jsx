e9c564b525a49e6d2e179905e686c520b7530cc2
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-03-06 19:35:31 +0530

    conditional layout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+  const pathName = usePathname();
+
   return (
     <div>
         <ul className='flex gap-5'>
@@ -10,6 +15,12 @@ const Navbar = () => {
             <Link href="/portfolio"><li>Portfolio</li></Link>
             <Link href="/about/user"><li>User</li></Link>
         </ul>
+
+        {pathName=="/about"?(
+          <button className="p-2 m-1 bg-blue-500 rounded-md">Inside about</button>
+        ):(
+          <button className="p-2 m-1 bg-blue-500 rounded-md">outside about page</button>
+        )}
     </div>
   )
 }
