b31faec76d2437a435ed11e417a9b34203a55816
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-03-07 19:28:00 +0530

    Rendering external images

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -6,6 +6,10 @@ const Portfolio = () => {
   return (
     <div>this is Portfolio
     <Image src={myImage} alt="Portfolio" width="500" height="500"/>
+    <Image src='https://plus.unsplash.com/premium_photo-1671247953201-2fdc17af6692?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8' alt='Laptop image'
+      width={300}
+      height={300}
+    />
     </div>
   )
 }
