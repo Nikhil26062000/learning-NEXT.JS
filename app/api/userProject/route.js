8e670a902ec12ec301cbc2c8348f9f3b0f7cdae9
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-03-18 15:13:35 +0530

    -INSERTING NEW USER IN DATA

--- app/api/userProject/route.js
+++ app/api/userProject/route.js
@@ -1,9 +1,33 @@
-
-
-import {data} from "@/app/util/db"
-import { NextResponse } from "next/server"
+import { data } from "@/app/util/db";
+import { NextResponse } from "next/server";
+import fs from "fs";
 
 // fetching all the data from the fake db
-export function GET(){
-    return NextResponse.json({message: data})
+export function GET() {
+  return NextResponse.json({ message: data });
+}
+
+//inserting the new user in db.js
+export async function POST(req,res) {
+  let { id, name, email, password,age } = await req.json();
+
+  if (!id || !name || !email || !password || !age) {
+    return NextResponse.json({ message: "Please fill all the details" });
+  } else {
+    data.push({ id: id, name: name, email: email, password, age: age });
+
+    const updatedUserArray = data;
+
+    const updatedData = JSON.stringify(updatedUserArray, null, 2);
+     // Null is passed as the second argument to exclude any replacer function
+            // 2 is passed as the third argument to specify 2 spaces for indentation
+
+    fs.writeFileSync(
+      "./app/util/db.js",
+      `export const data = ${updatedData}`,
+      "utf-8"
+    );
+  }
+
+  return NextResponse.json({message:"User insert Successful"})
 }
--- app/util/db.js
+++ app/util/db.js
@@ -1,21 +1,30 @@
-
-
 export const data = [
-    {
-        id: '1',
-        name: 'John',
-        age: 21,
-        email: 'john@example.com',
-        password: 'password@123'
-
-    },
-
-    {
-        id: '2',
-        name: 'Doe',
-        age: 31,
-        email: 'Doe@example.com',
-        password: 'doe@123'
-
-    }
+  {
+    "id": "1",
+    "name": "John",
+    "age": 21,
+    "email": "john@example.com",
+    "password": "password@123"
+  },
+  {
+    "id": "2",
+    "name": "Doe",
+    "age": 31,
+    "email": "Doe@example.com",
+    "password": "doe@123"
+  },
+  {
+    "id": "2345",
+    "name": "Nikhil",
+    "email": "nik@gamil.com",
+    "password": "12345",
+    "age": "23"
+  },
+  {
+    "id": "235",
+    "name": "Ayush",
+    "email": "ayu@gamil.com",
+    "password": "125",
+    "age": "18"
+  }
 ]
